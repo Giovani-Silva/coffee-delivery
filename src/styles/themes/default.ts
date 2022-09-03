252b77a2d5054da13d26f9529fb593702eb3e341
Giovani Silva <giovani.silva@live.com>
2022-09-03 20:25:03 -0300

    fonte e cores do tema

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+  background: '#FAFAFA',
+
+  'gray-100': '#F3F2F2',
+  'gray-300': '#EDEDED',
+  'gray-400': '#E6E5E5',
+  'gray-500': '#D7D5D5',
+  'gray-600': '#8D8686',
+  'gray-700': '#574F4D',
+  'gray-800': '#403937',
+  'gray-900': '#272221',
+
+  'primary-100': '#F1E9C9',
+  'primary-400': '#DBAC2C',
+  'primary-500': '#C47F17',
+
+  'secondary-100': '#EBE5F9',
+  'secondary-400': '#8047F8',
+  'secondary-500': '#4B2995',
+};
