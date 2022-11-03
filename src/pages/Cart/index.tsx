2f3409cc6320064764cd7d8a94a0378c65081c15
Giovani Silva <giovani.silva@live.com>
2022-11-03 20:02:36 -0300

    card box

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,3 +1,12 @@
 export function Cart() {
-  return <h1>Complete seu pedido</h1>;
+  return (
+    <>
+      <h1>Complete seu pedido</h1>
+
+      <div>
+        <h2>Endereço de Entrega</h2>
+        <p>Informe o endreço onde deseja receber seu pedido</p>
+      </div>
+    </>
+  );
 }
